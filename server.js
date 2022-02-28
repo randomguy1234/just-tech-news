fef55b4e041f52ffacdf4fd9f01a089d8c96febc
Jimmy Rando <jsturat46925@gmail.com>
2022-02-28 17:56:37 -0500

    rush added vote model and routes

--- server.js
+++ server.js
@@ -1,18 +1,17 @@
-const express= require('express');
-const routes= require('./routes');
-const sequelize= require('./config/connection');
+const express = require('express');
+const routes = require('./routes');
+const sequelize = require('./config/connection');
 
-const app= express();
-const PORT= process.env.PORT || 3001;
+const app = express();
+const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
-//turn on routes
+// turn on routes
 app.use(routes);
 
-//turn on connection to db and server
-sequelize.sync({force: false}).then(()=>
-{
-    app.listen(PORT, () => console.log('Now listening'));
+// turn on connection to db and server
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log('Now listening'));
 });
